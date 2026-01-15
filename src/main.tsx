ce748a29d12fa152c54704f877bfe5fc4a22725a
ntu09082007-eng <ntu09082007@gmail.com>
2026-01-15 17:52:04 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,15 @@
-import React from 'react';
+import React from 'react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+
+// --- ĐOẠN NÀY LÀ QUAN TRỌNG NHẤT ---
+// Ép React vào biến toàn cục để sửa lỗi "React is not defined" bất chấp cấu hình
+if (typeof window !== 'undefined') {
+  (window as any).React = React;
+}
+// -----------------------------------
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
