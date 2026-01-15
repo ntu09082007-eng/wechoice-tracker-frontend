03a55edfeb3ba43ade976704d0a405a01536dbbb
ntu09082007-eng <ntu09082007@gmail.com>
2026-01-15 21:32:58 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,30 @@
-import React from 'react';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
-import RealtimePage from "./pages/RealtimePage";
-import StatsPage from "./pages/StatsPage";
-import PredictionPage from "./pages/PredictionPage";
-import AboutPage from "./pages/AboutPage";
-// 👇 1. IMPORT TRANG CHART MỚI TẠO Ở ĐÂY
-import ChartPage from "./pages/ChartPage"; 
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import RealtimePage from './pages/RealtimePage';
+import StatsPage from './pages/StatsPage';
+import PredictionPage from './pages/PredictionPage';
+import AboutPage from './pages/AboutPage';
+import EmptyPage from './pages/EmptyPage';
+import ChartPage from './pages/ChartPage'; // Giữ lại trang Chart mới
 
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import EmptyPage from "./pages/EmptyPage";
-
-export default function App() {
+function App() {
   return (
-    <Router>
-      {/* Layout chính giữ nguyên style của bạn */}
-      <div className="min-h-screen bg-white flex flex-col overflow-x-hidden">
-        <Header />
-        <main className="flex-grow w-full mb-[20px] min-h-[calc(100vh-80px)]">
-          <div className="w-full px-4 md:px-6 py-8">
-            <Routes>
-              {/* Trang chủ mặc định chuyển hướng về realtime */}
-              <Route index element={<Navigate to="/realtime" replace />} />
-              <Route path="/realtime" element={<RealtimePage />} />
-              
-              {/* 👇 2. ĐỔI THÀNH /history ĐỂ KHỚP VỚI MENU "THỐNG KÊ" */}
-              <Route path="/history" element={<StatsPage />} />
-              
-              {/* 👇 3. THÊM ROUTE MỚI CHO MENU "BIỂU ĐỒ" */}
-              <Route path="/chart" element={<ChartPage />} />
-
-              <Route path="/prediction" element={<PredictionPage />} />
-              <Route path="/about" element={<AboutPage />} />
-              
-              {/* Trang 404 */}
-              <Route path="*" element={<EmptyPage />} />
-            </Routes>
-          </div>
-        </main>
-        <Footer />
-      </div>
-    </Router>
+    <BrowserRouter>
+      <Routes>
+        {/* Chỉ định tuyến thôi, KHÔNG bọc Header/Footer ở đây nữa */}
+        <Route path="/" element={<Navigate to="/realtime" replace />} />
+        <Route path="/realtime" element={<RealtimePage />} />
+        
+        {/* Các trang chức năng */}
+        <Route path="/history" element={<StatsPage />} />
+        <Route path="/chart" element={<ChartPage />} />
+        <Route path="/prediction" element={<PredictionPage />} />
+        <Route path="/about" element={<AboutPage />} />
+        
+        {/* Trang lỗi 404 */}
+        <Route path="*" element={<EmptyPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
+
+export default App;
