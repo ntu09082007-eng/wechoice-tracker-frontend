4ab51411366e49a9f638539c9ed169e6ce254688
ntu09082007-eng <ntu09082007@gmail.com>
2026-01-15 17:53:58 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      // Dòng này cực quan trọng: Ép buộc sử dụng Runtime mới của React 19
-      // Nó sẽ tự động tiêm React vào mọi nơi, bạn không cần import tay nữa.
-      jsxRuntime: 'automatic', 
-    })
-  ],
+  plugins: [react()],
 })
