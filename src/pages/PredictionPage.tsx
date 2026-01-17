64789df3259fd531079ab3b908b975a239a934ea
ntu09082007-eng <ntu09082007@gmail.com>
2026-01-17 15:23:27 +0700

    Update PredictionPage.tsx

--- src/pages/PredictionPage.tsx
+++ src/pages/PredictionPage.tsx
@@ -3,7 +3,7 @@ export default function PredictionPage() {
     <div className="px-4 md:px-8 py-10 max-w-[98%] mx-auto">
       <div className="mb-10 text-center">
         <h1 className="text-4xl md:text-5xl font-bold  mb-8">
-          Dự đoán thời gian bắt kịp
+          Thời gian bắt kịp
         </h1>
 
         <div className="text-gray-600 text-lg md:text-xl">
